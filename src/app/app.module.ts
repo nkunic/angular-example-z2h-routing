cd7849e6dcc1d5db79da9b3a998b67ff6f13e86f
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-28 11:50:55 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
+    ProductsComponent,
+    ProductComponent,
     ContactComponent,
     NavigationComponent,
   ],
